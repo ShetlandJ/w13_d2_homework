83a77da8ee8dbeaf25359d4b140bf4fcfc252568
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-12 15:09:01 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var parser = require('body-parser');
+var app = express();
+var path = require("path");
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static('client/build'));
+
+var MongoClient = require("mongodb").MongoClient
+
+MongoClient.connect("mongodb://localhost:27017/star_wars", function(err, database){
+  if(err){
+    return console.log(err);
+  }
+
+  db = database;
+  console.log("Connected to db");
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+
+});
