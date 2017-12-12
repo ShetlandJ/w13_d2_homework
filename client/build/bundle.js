5e3dd1ee85246f9e1c8b9d2e6c8a00dc2a58e514
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-12 16:57:29 +0000

    added delete btn

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -115,15 +115,32 @@ BandView.prototype = {
 
     console.log(bands);
     bands.forEach( function(band){
-      var li = document.createElement('li');
-      var text = document.createElement('p');
-      var div = document.getElementById('artist-info');
-      text.innerText = band.name + ": " + band.genre;
-      li.appendChild(text);
-      div.appendChild(li);
+      var bandName = document.createElement('td');
+      bandName.innerText = band.name;
+      var genre = document.createElement('td');
+      genre.innerText = band.genre;
+      var deleteBtn = document.createElement('td');
+      var editBtn = document.createElement('td');
+      var tableRow = document.createElement('tr');
+      var dButton = document.createElement('button');
+
+      dButton.innerText = "❌"
+
+      deleteBtn.appendChild(dButton);
+
+      tableRow.appendChild(bandName);
+      tableRow.appendChild(genre);
+      tableRow.appendChild(deleteBtn);
+      tableRow.appendChild(editBtn);
+
+      // var div = document.getElementById('artist-info');
+      var table = document.getElementById('artist-table');
+      table.appendChild(tableRow);
+      // div.appendChild(li);
     })
   }
 }
+
 
  module.exports = BandView;
 
